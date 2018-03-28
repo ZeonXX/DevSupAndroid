c15a53dcc7ab6fd886fbfeebcedb4b181fdb5947
ZeonXX <zeooondevel@gmail.com>
2018-03-28 22:41:29 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/app/DEVELOP/TODO.java
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/app/DEVELOP/TODO.java
@@ -2,7 +2,6 @@ package com.sup.dev.android.app.DEVELOP;
 
 public interface TODO {
 
-
     //  TODO Адаптер с подгрузкой из нескольких запросов
     //  TODO Идикатор пейджера с заголовками
     //  TODO LayoutExpandable - Меняет размер не анемировнно
