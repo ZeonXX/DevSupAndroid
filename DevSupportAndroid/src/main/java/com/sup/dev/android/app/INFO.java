8f0d66ae25efbb4b2f4ad80e2f8e860a41653c3b
Dmitriy <zeooondevel@gmail.com>
2018-08-16 14:53:26 +0700

    Update INFO.java

--- DevSupportAndroid/src/main/java/com/sup/dev/android/app/INFO.java
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/app/INFO.java
@@ -28,6 +28,25 @@ public interface INFO {
             View decor = getWindow().getDecorView();
             decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
+        
+        
+        
+        Android API note
+        14: 4.0     IceCreamSandwich
+        15: 4.0.3   IceCreamSandwich
+        16. 4.1     Jelly Bean
+        17. 4.2     Jelly Bean
+        18. 4.3     Jelly Bean
+        19. 4.4     KitKat
+        20. 4.4W    KitKat Wear
+        21. 5.0     LollyPop
+        22. 5.1     LollyPop
+        23. 6.0     Marshmallow
+        24. 7.0     Nougat
+        25. 7.1.1   Nougat
+        26. 8.0     Oreo
+        27. 8.1     Oreo
+        28. 9.0     Pie
 
     */
 
