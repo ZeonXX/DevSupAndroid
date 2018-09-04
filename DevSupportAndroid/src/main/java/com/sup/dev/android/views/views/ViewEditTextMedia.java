a4fa95760b7f9df0dce457dadeacc86675a849cd
Холин Дмитрий <kholin@beward.ru>
2018-09-04 12:57:54 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/views/ViewEditTextMedia.java
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/views/ViewEditTextMedia.java
@@ -13,7 +13,7 @@ import com.sup.dev.java.libs.debug.Debug;
 
 public class ViewEditTextMedia extends android.support.v7.widget.AppCompatEditText {
 
-    private Callback1<String> callbackl;
+    private Callback1<String> callback;
 
     public ViewEditTextMedia(Context context) {
         this(context, null);
@@ -28,28 +28,25 @@ public class ViewEditTextMedia extends android.support.v7.widget.AppCompatEditTe
         final InputConnection ic = super.onCreateInputConnection(editorInfo);
         EditorInfoCompat.setContentMimeTypes(editorInfo, new String[]{"image/*"});
 
-        return InputConnectionCompat.createWrapper(ic, editorInfo,  (inputContentInfo, flags, opts) -> {
+        return InputConnectionCompat.createWrapper(ic, editorInfo, (inputContentInfo, flags, opts) -> {
             if (BuildCompat.isAtLeastNMR1() && (flags & InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
                 try {
                     inputContentInfo.requestPermission();
                 } catch (Exception e) {
                     Debug.log(e);
-                    return false; // return false if failed
+                    return false;
                 }
             }
 
             Debug.log(inputContentInfo.getContentUri());
-            if(callbackl != null) callbackl.callback(inputContentInfo.getContentUri().toString());
-
-            // read and display inputContentInfo asynchronously.
-            // call inputContentInfo.releasePermission() as needed.
+            if (callback != null) callback.callback(inputContentInfo.getContentUri().getPath());
 
             return true;
         });
     }
 
-    public void setCallback(Callback1<String> callback){
-this.callbackl = callbackl;
+    public void setCallback(Callback1<String> callback) {
+        this.callback = callback;
     }
 
 
