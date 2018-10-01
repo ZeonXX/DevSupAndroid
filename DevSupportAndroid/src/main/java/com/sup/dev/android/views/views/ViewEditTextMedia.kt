42fef44a34391fa95a38d447570a0cbd6875687e
Холин Дмитрий <kholin@beward.ru>
2018-10-01 14:27:29 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/views/ViewEditTextMedia.kt
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/views/ViewEditTextMedia.kt
@@ -1,10 +1,8 @@
 package com.sup.dev.android.views.views
 
 import android.content.Context
-import android.os.Bundle
 import android.support.v13.view.inputmethod.EditorInfoCompat
 import android.support.v13.view.inputmethod.InputConnectionCompat
-import android.support.v13.view.inputmethod.InputContentInfoCompat
 import android.support.v4.os.BuildCompat
 import android.util.AttributeSet
 import android.view.inputmethod.EditorInfo
@@ -12,49 +10,40 @@ import android.view.inputmethod.InputConnection
 import com.sup.dev.java.libs.debug.Debug
 
 
-class ViewEditTextMedia @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : android.support.v7.widget.AppCompatEditText(context, attrs) {
+class ViewEditTextMedia constructor(context: Context, attrs: AttributeSet? = null) : android.support.v7.widget.AppCompatEditText(context, attrs) {
+
 
     private var callback: ((String) -> Unit)? = null
 
-    override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection {
+    override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection? {
         if (callback == null) return super.onCreateInputConnection(editorInfo)
         try {
-            var ic: InputConnection = super.onCreateInputConnection(editorInfo);
+            val ic: InputConnection? = super.onCreateInputConnection(editorInfo)
             EditorInfoCompat.setContentMimeTypes(editorInfo, arrayOf("image/*"))
 
-            var funct: (InputContentInfoCompat, Int, Bundle) -> Boolean = { inputContentInfo, flags, opts ->
-                if (BuildCompat.isAtLeastNMR1() &&
-                        (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+            return InputConnectionCompat.createWrapper(ic!!, editorInfo) { inputContentInfo, flags, opts ->
+                if (BuildCompat.isAtLeastNMR1() && (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
                     try {
-                        inputContentInfo.requestPermission();
-                    } catch (e: Exception) {
+                        inputContentInfo.requestPermission()
+                    } catch (e:Exception) {
                         Debug.log(e)
                         false
                     }
                 }
 
-                if (BuildCompat.isAtLeastNMR1() && (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
-                    try {
-                        inputContentInfo.requestPermission();
-                    } catch (e: Exception) {
-                        Debug.log(e)
-                        false
-                    }
-                }
                 try {
-                    if (callback != null) callback!!.invoke(inputContentInfo.getLinkUri().toString())
+                    if (callback != null) callback!!.invoke(inputContentInfo.linkUri.toString())
                     true
-                } catch (e: Exception) {
+                } catch (e:Exception) {
                     Debug.log(e)
                 }
+
                 false
             }
 
-            return InputConnectionCompat.createWrapper(ic, editorInfo, InputConnectionCompat.OnCommitContentListener(
-                    function = funct))
         } catch (e: Exception) {
             Debug.log(e)
-            return super.onCreateInputConnection(editorInfo);
+            return super.onCreateInputConnection(editorInfo)
         }
     }
 
