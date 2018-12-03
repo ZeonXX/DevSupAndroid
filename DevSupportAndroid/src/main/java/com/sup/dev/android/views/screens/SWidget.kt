0c931e0bef65c12d5d2edca1abc13706c8895599
Холин Дмитрий <kholin@beward.ru>
2018-12-03 17:26:44 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/screens/SWidget.kt
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/screens/SWidget.kt
@@ -1,5 +1,6 @@
 package com.sup.dev.android.views.screens
 
+import android.support.annotation.DrawableRes
 import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.ViewGroup
@@ -7,16 +8,28 @@ import com.sup.dev.android.R
 import com.sup.dev.android.libs.screens.Screen
 import com.sup.dev.android.libs.screens.navigator.Navigator
 import com.sup.dev.android.tools.ToolsView
+import com.sup.dev.android.views.views.ViewIcon
 import com.sup.dev.android.views.widgets.Widget
 import com.sup.dev.android.views.widgets.WidgetViewWrapper
 
 class SWidget(private val widget: Widget) : Screen(R.layout.screen_widget), WidgetViewWrapper {
 
     private val vContainer: ViewGroup = findViewById(R.id.vContainer)
+    private val vToolbarContainer: ViewGroup = findViewById(R.id.vToolbarContainer)
 
     init {
         vContainer.addView(ToolsView.removeFromParent(widget.view))
     }
+
+
+    fun addToolbarIcon(@DrawableRes res: Int, onClick: (View) -> Unit): ViewIcon {
+        val viewIcon: ViewIcon = ToolsView.inflate(context, R.layout.view_icon)
+        viewIcon.setImageResource(res)
+        viewIcon.setOnClickListener { onClick.invoke(viewIcon) }
+        vToolbarContainer.addView(viewIcon)
+        return viewIcon
+    }
+
 
     override fun setTitle(title: String?) {
         (findViewById<View>(R.id.vToolbar) as Toolbar).title = title
