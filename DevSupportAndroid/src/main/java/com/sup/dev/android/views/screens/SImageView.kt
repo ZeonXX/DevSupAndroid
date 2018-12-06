fc3fcf04e271bd682f59bc2098afd8125788c0f1
Холин Дмитрий <kholin@beward.ru>
2018-12-06 12:57:41 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/screens/SImageView.kt
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/screens/SImageView.kt
@@ -1,67 +1,108 @@
 package com.sup.dev.android.views.screens
 
 import android.graphics.Bitmap
+import android.support.v4.view.ViewPager
+import android.view.View
 import android.widget.ImageView
 import com.sup.dev.android.R
 import com.sup.dev.android.app.SupAndroid
 import com.sup.dev.android.libs.screens.Screen
 import com.sup.dev.android.tools.*
+import com.sup.dev.android.views.cards.Card
 import com.sup.dev.android.views.support.DrawableGif
+import com.sup.dev.android.views.support.adapters.pager.PagerCardAdapter
 import com.sup.dev.android.views.views.ViewIcon
+import com.sup.dev.android.views.views.layouts.LayoutZoom
 import com.sup.dev.java.tools.ToolsBytes
 import com.sup.dev.java.tools.ToolsThreads
 
 
-class SImageView private constructor(
-        private val bitmap: Bitmap?,
-        private val id: Long)
+class SImageView private constructor()
     : Screen(R.layout.screen_image_view) {
 
-    constructor(bitmap: Bitmap) : this(bitmap, 0)
+    private val vPager: ViewPager = findViewById(R.id.vPager)
+    private val vDownload: ViewIcon = findViewById(R.id.vDownload)
+    private val adapterIn: PagerCardAdapter = PagerCardAdapter()
 
-    constructor(id: Long) : this(null, id)
+    constructor(vararg bitmaps: Bitmap) : this() {
+        for (b in bitmaps) adapterIn.add(Page(b, 0L))
+    }
+
+    constructor(vararg ids: Long) : this() {
+        for (id in ids) adapterIn.add(Page(null, id))
+    }
 
     init {
-
         isBottomNavigationVisible = false
         isBottomNavigationAllowed = false
         isBottomNavigationAnimation = false
 
-        val vImage: ImageView = findViewById(R.id.vImage)
-        val vDownload: ViewIcon = findViewById(R.id.vDownload)
-
-        vImage.isClickable = false
         vDownload.setOnClickListener { v -> download() }
+        vPager.adapter = adapterIn
+        vPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
 
-        if (bitmap != null)
-            vImage.setImageBitmap(bitmap)
-        else if (id > 0)
-            ToolsImagesLoader.load(id).into { bytes ->
-                if (bytes != null) {
-                    if (ToolsBytes.isGif(bytes)) DrawableGif(bytes, vImage) { vImage.setImageDrawable(it) }
-                    else vImage.setImageBitmap(ToolsBitmap.decode(bytes))
-                }
+            override fun onPageScrollStateChanged(p0: Int) {
+                for (i in 0 until adapterIn.size()) (adapterIn[i] as Page).update()
             }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+            }
+
+            override fun onPageSelected(p0: Int) {
+            }
+
+        })
     }
 
     private fun download() {
-        val dialog = ToolsView.showProgressDialog(R.string.app_downloading)
-        ToolsThreads.thread {
-            if (bitmap != null)
-                ToolsStorage.saveImageInDownloadFolder(bitmap)
-            else if (id > 0) {
-                ToolsImagesLoader.load(id).into { bytes ->
-                    if (!ToolsBytes.isGif(bytes))
-                        ToolsStorage.saveImageInDownloadFolder(ToolsBitmap.decode(bytes)!!) { f -> ToolsToast.show(R.string.app_done) }
-                    else
-                        ToolsStorage.saveFileInDownloadFolder(bytes!!, ".gif", { f -> ToolsToast.show(R.string.app_done) }, { ToolsToast.show(R.string.error_permission_files) })
+        (adapterIn[vPager.currentItem] as Page).download()
+    }
 
+
+    private class Page constructor(
+            private val bitmap: Bitmap?,
+            private val id: Long
+    ) : Card() {
+
+        override fun getLayout() = R.layout.screen_image_view_page
+
+        override fun bindView(view: View) {
+            val vImage: ImageView = view.findViewById(R.id.vImage)
+            val vZoom: LayoutZoom = view.findViewById(R.id.vZoom)
+
+            vZoom.reset()
+            vImage.isClickable = false
+
+            if (bitmap != null)
+                vImage.setImageBitmap(bitmap)
+            else if (id > 0)
+                ToolsImagesLoader.load(id).into { bytes ->
+                    if (bytes != null) {
+                        if (ToolsBytes.isGif(bytes)) DrawableGif(bytes, vImage) { vImage.setImageDrawable(it) }
+                        else vImage.setImageBitmap(ToolsBitmap.decode(bytes))
+                    }
                 }
-            }
-            dialog.hide()
-            ToolsToast.show(R.string.app_downloaded)
         }
 
+        fun download() {
+            val dialog = ToolsView.showProgressDialog(R.string.app_downloading)
+            ToolsThreads.thread {
+                if (bitmap != null)
+                    ToolsStorage.saveImageInDownloadFolder(bitmap)
+                else if (id > 0) {
+                    ToolsImagesLoader.load(id).into { bytes ->
+                        if (!ToolsBytes.isGif(bytes))
+                            ToolsStorage.saveImageInDownloadFolder(ToolsBitmap.decode(bytes)!!) { f -> ToolsToast.show(R.string.app_done) }
+                        else
+                            ToolsStorage.saveFileInDownloadFolder(bytes!!, ".gif", { f -> ToolsToast.show(R.string.app_done) }, { ToolsToast.show(R.string.error_permission_files) })
+
+                    }
+                }
+                dialog.hide()
+                ToolsToast.show(R.string.app_downloaded)
+            }
+
+        }
     }
 
 }
