b6f2a673f9bd8c5f3d95ea904c83e86b21f626a7
zeooondevel <zeooondevel@gmail.com>
2018-12-08 11:30:54 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/views/ViewErrorEditText.kt
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/views/ViewErrorEditText.kt
@@ -3,10 +3,9 @@ package com.sup.dev.android.views.views
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import com.sup.dev.java.libs.debug.log
 
 
-open class ViewErrorEditText : android.support.design.widget.TextInputEditText {
+open class ViewErrorEditText : android.support.v7.widget.AppCompatEditText {
 
     constructor(context: Context) : super(context)
 
@@ -15,7 +14,6 @@ open class ViewErrorEditText : android.support.design.widget.TextInputEditText {
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
 
     override fun setError(pError: CharSequence?, pIcon: Drawable?) {
-        log("setError [$pError] [$pIcon]")
         when {
             pError == null -> super.setError(null, null)
             pError.isEmpty() -> setCompoundDrawables(null, null, pIcon, null)
