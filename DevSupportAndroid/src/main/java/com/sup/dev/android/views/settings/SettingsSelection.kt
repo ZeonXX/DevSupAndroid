aba761ef0906aa40856e8a5d23e9a143ff27526a
Холин Дмитрий <kholin@beward.ru>
2019-04-19 12:29:32 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/settings/SettingsSelection.kt
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/settings/SettingsSelection.kt
@@ -2,17 +2,50 @@ package com.sup.dev.android.views.settings
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
+import com.sup.dev.android.R
+import com.sup.dev.android.tools.ToolsResources
 import com.sup.dev.android.views.widgets.WidgetMenu
 
-class SettingsSelection  constructor(context: Context, attrs: AttributeSet? = null) : Settings(context, attrs) {
+class SettingsSelection constructor(context: Context, attrs: AttributeSet? = null) : Settings(context, attrs) {
 
-    val menu: WidgetMenu = WidgetMenu()
+    private val menu: WidgetMenu = WidgetMenu()
+    private var currentIndex = 0
+    private val list = ArrayList<String>()
+    private var onSelected: (Int) -> Unit = {}
+    private val vArrow: ImageView = ImageView(context)
+
 
     init {
+        view.setOnClickListener { menu.asSheetShow() }
+        vArrow.setImageDrawable(ToolsResources.getDrawableFromAttr(R.attr.ic_keyboard_arrow_down))
 
-        view.setOnClickListener{ menu.asSheetShow()}
-        menu.setOnGlobalSelected { w, t -> setSubtitle(t) }
+        setSubView(vArrow)
+    }
 
+    fun add(v: Int, onItemSelected: (Int) -> Unit = {}) {
+        add(ToolsResources.s(v), onItemSelected)
+    }
+
+    fun add(v: String, onItemSelected: (Int) -> Unit = {}) {
+        list.add(v)
+        menu.add(v) { w, i ->
+            currentIndex = list.indexOf(v)
+            setSubtitle(v)
+            onItemSelected.invoke(currentIndex)
+            onSelected.invoke(currentIndex)
+        }
+    }
+
+    fun getCurrentIndex() = currentIndex
+
+    fun setCurrentIndex(index: Int) {
+        currentIndex = index
+        setSubtitle(list[index])
+    }
+
+    fun onSelected(onSelected: (Int) -> Unit) {
+        this.onSelected = onSelected
     }
 
 }
