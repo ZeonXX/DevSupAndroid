294058eca9fbbbf7f1a7dd047183a8b9d3a1354c
Холин Дмитрий <kholin@beward.ru>
2019-05-15 12:58:08 +0700

    Commit

--- DevSupportAndroid/src/main/java/com/sup/dev/android/views/screens/SLoadingRecycler.kt
+++ DevSupportAndroid/src/main/java/com/sup/dev/android/views/screens/SLoadingRecycler.kt
@@ -21,7 +21,7 @@ abstract class SLoadingRecycler<C : Card, V>(res: Int = R.layout.screen_loading_
 
     protected var textErrorRetry = SupAndroid.TEXT_APP_RETRY
 
-    protected val vToolbar: Toolbar = findViewById(R.id.vToolbar)
+    protected val vToolbar: Toolbar? = findViewById(R.id.vToolbar)
     protected val vToolbarIconsContainer: ViewGroup? = findViewById(R.id.vToolbarIconsContainer)
     protected val vRecycler: RecyclerView = findViewById(R.id.vRecycler)
     protected val vRefresh: SwipeRefreshLayout? = findViewById(R.id.vRefresh)
@@ -96,7 +96,7 @@ abstract class SLoadingRecycler<C : Card, V>(res: Int = R.layout.screen_loading_
     }
 
     protected fun addToolbarView(v: View) {
-        vToolbar.addView(v)
+        vToolbar?.addView(v)
     }
 
     //
