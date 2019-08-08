49d031491c8f3da4667c8e01893dc1206b5451d0
Холин Дмитрий <kholin@beward.ru>
2019-08-08 12:51:06 +0700

    Commit

--- src/main/java/com/sup/dev/android/utils/UtilsAudioPlayer.kt
+++ src/main/java/com/sup/dev/android/utils/UtilsAudioPlayer.kt
@@ -1,15 +1,25 @@
 package com.sup.dev.android.utils
 
+import android.content.Context
 import android.media.AudioFormat
 import android.media.AudioManager
+import android.media.AudioManager.STREAM_MUSIC
 import android.media.AudioTrack
+import com.sup.dev.java.libs.debug.log
 import com.sup.dev.java.tools.ToolsThreads
+import android.content.Context.AUDIO_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import com.sup.dev.android.app.SupAndroid
+import com.sup.dev.java.libs.debug.err
+import java.lang.IllegalStateException
+
 
 class UtilsAudioPlayer {
 
-    var onStep: (Long)-> Unit = {}
+    var onStep: (Long) -> Unit = {}
     private val stepTime = 100L
     private val sampleRate = 8000
+    private val frameSize = 160
     private val minBufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
 
     private var subPlayer: SubPlayer? = null
@@ -37,23 +47,30 @@ class UtilsAudioPlayer {
             onStop: () -> Unit
     ) {
 
-        val audioTrack = AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize, AudioTrack.MODE_STREAM)
+        val audioTrack = AudioTrack(STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize, AudioTrack.MODE_STREAM)
         var stop = false
 
         init {
             ToolsThreads.thread {
                 var offset = 0
-                val startTime =  System.currentTimeMillis()
-                var lastStep = startTime
+
                 audioTrack.play()
-                while (!stop && offset < byteArray.size) {
-                    audioTrack.write(byteArray, offset, 160)
-                    offset += 160
-                    ToolsThreads.sleep(10)
-                    if(lastStep + stepTime < System.currentTimeMillis()){
-                        lastStep = System.currentTimeMillis()
-                        ToolsThreads.main {onStep.invoke(System.currentTimeMillis() - startTime)}
+                try {
+                    while (!stop && offset < byteArray.size) {
+                        if (audioTrack.playState == AudioTrack.PLAYSTATE_PLAYING) {
+                            audioTrack.write(byteArray, offset, frameSize)
+                            offset += frameSize
+                            ToolsThreads.main {
+                                try {
+                                    onStep.invoke(audioTrack.getPlaybackHeadPosition() / (sampleRate/1000L))
+                                } catch (e: Exception) {
+                                    if(e !is IllegalStateException)err(e)
+                                }
+                            }
+                        }
                     }
+                } catch (e: Exception) {
+                    if(e !is IllegalStateException)err(e)
                 }
                 audioTrack.stop()
                 audioTrack.release()
