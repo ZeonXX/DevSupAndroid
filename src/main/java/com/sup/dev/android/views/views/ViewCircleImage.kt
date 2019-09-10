0e98586c79026eb8da14215cc034dfebe14cf6a4
Холин Дмитрий <kholin@beward.ru>
2019-09-10 11:41:17 +0700

    Commit

--- src/main/java/com/sup/dev/android/views/views/ViewCircleImage.kt
+++ src/main/java/com/sup/dev/android/views/views/ViewCircleImage.kt
@@ -22,8 +22,8 @@ class ViewCircleImage constructor(context: Context, attrs: AttributeSet? = null)
     private var useGlobalStyle = true
     private var backgroundColorCircle = 0x00000000
     private val path = Path()
-    private var ww = -1
-    private var hh = -1
+   //private var ww = -1
+   //private var hh = -1
 
     private val eventBus = EventBus.subscribe(EventStyleChanged::class){
         if(useGlobalStyle) {
@@ -38,27 +38,24 @@ class ViewCircleImage constructor(context: Context, attrs: AttributeSet? = null)
     }
 
     override fun draw(canvas: Canvas) {
-        if (ww != width || hh != height) {
-            this.ww = ww
-            this.hh = hh
 
-            if (squareMode) {
-                path.reset()
-                val dp = ToolsMath.min(squareCorned, height/2f)
-                path.addCircle(dp, dp, dp, Path.Direction.CCW)
-                path.addCircle(width - dp, dp, dp, Path.Direction.CCW)
-                path.addCircle(dp, height - dp, dp, Path.Direction.CCW)
-                path.addCircle(width - dp, height - dp, dp, Path.Direction.CCW)
-                path.addRect(0f, dp, width + 0f, height - dp, Path.Direction.CCW)
-                path.addRect(dp, 0f, width - dp, dp, Path.Direction.CCW)
-                path.addRect(dp, height - dp, width - dp, height + 0f, Path.Direction.CCW)
-                canvas.clipPath(path)
-            } else {
-                path.reset()
-                path.addCircle((width / 2).toFloat(), (height / 2).toFloat(), (Math.min(width, height) / 2).toFloat(), Path.Direction.CCW)
-                canvas.clipPath(path)
-            }
+        if (squareMode) {
+            path.reset()
+            val dp = ToolsMath.min(squareCorned, height/2f)
+            path.addCircle(dp, dp, dp, Path.Direction.CCW)
+            path.addCircle(width - dp, dp, dp, Path.Direction.CCW)
+            path.addCircle(dp, height - dp, dp, Path.Direction.CCW)
+            path.addCircle(width - dp, height - dp, dp, Path.Direction.CCW)
+            path.addRect(0f, dp, width + 0f, height - dp, Path.Direction.CCW)
+            path.addRect(dp, 0f, width - dp, dp, Path.Direction.CCW)
+            path.addRect(dp, height - dp, width - dp, height + 0f, Path.Direction.CCW)
+            canvas.clipPath(path)
+        } else {
+            path.reset()
+            path.addCircle((width / 2).toFloat(), (height / 2).toFloat(), (Math.min(width, height) / 2).toFloat(), Path.Direction.CCW)
+            canvas.clipPath(path)
         }
+
         if (backgroundColorCircle != 0x00000000)
             canvas.drawColor(backgroundColorCircle)
         super.draw(canvas)
@@ -66,8 +63,8 @@ class ViewCircleImage constructor(context: Context, attrs: AttributeSet? = null)
 
     fun setSquareMode(squareMode: Boolean) {
         this.squareMode = squareMode
-        this.ww = -1
-        this.hh = -1
+     //  this.ww = -1
+     //  this.hh = -1
         invalidate()
     }
 
