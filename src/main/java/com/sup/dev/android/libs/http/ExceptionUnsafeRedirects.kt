4bb5ce65d760327c46dc7c3e350d158757505c89
zeooondevel <zeooondevel@gmail.com>
2019-10-21 12:57:47 +0700

    Commit

--- src/main/java/com/sup/dev/android/libs/http/ExceptionUnsafeRedirects.kt
+++
@@ -1,5 +0,0 @@
-package com.sup.dev.android.libs.http
-
-import java.io.IOException
-
-class ExceptionUnsafeRedirects : IOException("Unsafe redirect. To follow unsafe redirects, set followUnsafeRedirects to 'true'")
