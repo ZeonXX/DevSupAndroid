160b3c379575160d3f762e2e7724f7deccca07bc
Холин Дмитрий <kholin@beward.ru>
2019-11-29 17:10:28 +0700

    Commit

--- src/main/java/com/sup/dev/android/tools/ToolsApi.kt
+++ src/main/java/com/sup/dev/android/tools/ToolsApi.kt
@@ -1,49 +1,15 @@
 package com.sup.dev.android.tools
 
-import com.sup.dev.android.libs.screens.Screen
-import com.sup.dev.android.libs.screens.navigator.NavigationAction
-import com.sup.dev.android.libs.screens.navigator.Navigator
-import com.sup.dev.android.views.screens.SAlert
-import com.sup.dev.android.views.splash.Splash
-import com.sup.dev.android.views.splash.SplashProgressTransparent
 import com.sup.dev.java.libs.api.ApiRequest
-import com.sup.dev.java.libs.api.ApiResult
+import com.sup.dev.java.libs.api.ApiResponse
 
 object ToolsApi {
 
-    fun send(request: ApiRequest){
-        request.send()
-    }
-
-    fun sendProgressDialog(request: ApiRequest){
-        sendProgressDialog(ToolsView.showProgressDialog(), request)
-    }
-
-    fun sendProgressDialog(dLoading: Splash, request: ApiRequest){
+    fun <K:ApiResponse> sendProgressDialog(request: ApiRequest<K>){
+        val dLoading = ToolsView.showProgressDialog()
         request
                 .onFinish { dLoading.hide() }
                 .send()
-    }
-
-    fun sendSplash(action: NavigationAction, request: ApiRequest, onComplete: (ApiResult) -> Screen){
-        val vProgress = SplashProgressTransparent()
-        vProgress.asSplashShow()
-
-        send(request.onComplete {
-            if (vProgress.isHided()) return@onComplete
-            Navigator.action(action, onComplete.invoke(it))
-        }.onError {
-            if(vProgress.isHided()) return@onError
-            SAlert.showNetwork(action) {
-                Navigator.remove(it)
-                sendSplash(action, request, onComplete)
-            }
-        }.onFinish {
-            vProgress.hide()
-        })
-
-
-
     }
 
 }
