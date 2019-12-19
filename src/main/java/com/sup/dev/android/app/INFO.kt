264f0bfba866443231c31daece6adeceaadcb362
Холин Дмитрий <kholin@beward.ru>
2019-12-19 15:09:51 +0700

    Commit

--- src/main/java/com/sup/dev/android/app/INFO.kt
+++ src/main/java/com/sup/dev/android/app/INFO.kt
@@ -1,60 +1,31 @@
 package com.sup.dev.android.app
 
-
-interface INFO
-
     /*
-
-
-
-     Фикс для тестов:
-     -noverify
-
-
-     Git
-
 
      git submodule init
      
      git submodule update
 
-     git submodule add https://github.com/ZeonXX/DevSupJava.git
-     git submodule add https://github.com/ZeonXX/DevSupJavaPc.git
-     git submodule add https://github.com/ZeonXX/DevSupAndroid.git
-     git submodule add https://github.com/ZeonXX/DevSupEngineering.git
-     git submodule add https://github.com/ZeonXX/DevSupAndroidGoogle.git
-     git submodule add https://github.com/ZeonXX/CampfireSDK.git
-     git submodule add https://github.com/ZeonXX/CampfireApi.git
-     git submodule add https://github.com/ZeonXX/CampfireApiMedia.git
+     git submodule add PATCH
 
-     git rm -rf path_to_submodule
+     git rm -rf NAME
 
 
-    -   Тема
-
-    Цвет иконок в статусбаре
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            View decor = getWindow().getDecorView();
-            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-
-
-
-        Android API note
-        14: 4.0     IceCreamSandwich
-        15: 4.0.3   IceCreamSandwich
-        16. 4.1     Jelly Bean
-        17. 4.2     Jelly Bean
-        18. 4.3     Jelly Bean
-        19. 4.4     KitKat
-        20. 4.4W    KitKat Wear
-        21. 5.0     LollyPop
-        22. 5.1     LollyPop
-        23. 6.0     Marshmallow
-        24. 7.0     Nougat
-        25. 7.1.1   Nougat
-        26. 8.0     Oreo
-        27. 8.1     Oreo
-        28. 9.0     Pie
+     Android API note
+     14: 4.0     IceCreamSandwich
+     15: 4.0.3   IceCreamSandwich
+     16. 4.1     Jelly Bean
+     17. 4.2     Jelly Bean
+     18. 4.3     Jelly Bean
+     19. 4.4     KitKat
+     20. 4.4W    KitKat Wear
+     21. 5.0     LollyPop
+     22. 5.1     LollyPop
+     23. 6.0     Marshmallow
+     24. 7.0     Nougat
+     25. 7.1.1   Nougat
+     26. 8.0     Oreo
+     27. 8.1     Oreo
+     28. 9.0     Pie
 
     */
