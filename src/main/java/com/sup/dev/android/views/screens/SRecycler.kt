31dfd6d992bdca5405bf82cb6ec2ca4a51a13bc3
Dmitriy Kholin <zeooon@ya.ru>
2021-07-01 17:01:22 +0700

    first commit

---
+++ src/main/java/com/sup/dev/android/views/screens/SRecycler.kt
@@ -0,0 +1,40 @@
+package com.sup.dev.android.views.screens
+
+import androidx.annotation.DrawableRes
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.Toolbar
+import android.view.View
+import android.view.ViewGroup
+import com.sup.dev.android.R
+import com.sup.dev.android.libs.screens.Screen
+import com.sup.dev.android.tools.ToolsView
+import com.sup.dev.android.views.views.ViewIcon
+
+abstract class SRecycler(res: Int = R.layout.screen_recycler) : Screen(res) {
+
+    protected val vToolbar: Toolbar = findViewById(R.id.vToolbar)
+    protected val vToolbarIconsContainer: ViewGroup = findViewById(R.id.vToolbarIconsContainer)
+    protected val vRecycler: RecyclerView = findViewById(R.id.vRecycler)
+
+    init {
+        vRecycler.layoutManager = LinearLayoutManager(context)
+    }
+
+    fun onReloadClicked() {
+
+    }
+
+    protected fun addToolbarIcon(@DrawableRes res: Int, onClick: (View) -> Unit): ViewIcon {
+        val viewIcon: ViewIcon = ToolsView.inflate(context, R.layout.z_icon)
+        viewIcon.setImageResource(res)
+        viewIcon.setOnClickListener { onClick.invoke(viewIcon) }
+        vToolbarIconsContainer.addView(viewIcon)
+        return viewIcon
+    }
+
+    protected fun addToolbarView(v: View) {
+        vToolbar.addView(v)
+    }
+
+}
---
+++ src/main/java/com/sup/dev/android/views/settings/SettingsSeek.kt
@@ -0,0 +1,110 @@
+package com.sup.dev.android.views.settings
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import android.util.AttributeSet
+import android.view.View
+import android.widget.SeekBar
+import android.widget.TextView
+import com.sup.dev.android.R
+import com.sup.dev.android.tools.ToolsView
+import com.sup.dev.android.views.views.ViewIcon
+
+
+class SettingsSeek @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : Settings(context, attrs, R.layout.settings_seek), SeekBar.OnSeekBarChangeListener {
+
+    private val vSeekBar: SeekBar = findViewById(R.id.vDevSupSeekBar)
+
+    private var onProgressChanged: ((Int) -> Unit)? = null
+
+    //
+    //  Getters
+    //
+
+    var progress: Int
+        get() = vSeekBar.progress
+        set(progress) {
+            vSeekBar.progress = progress
+        }
+
+    //
+    //  Dpad
+    //
+
+    private var dpadStep = 1
+
+    init {
+
+        vSeekBar.id = View.NO_ID  //   Чтоб система не востонавливала состояние
+
+        val a = context.obtainStyledAttributes(attrs, R.styleable.SettingsSeek, 0, 0)
+        val maxProgress = a.getInteger(R.styleable.SettingsSeek_Settings_maxProgress, 100)
+        val progress = a.getInteger(R.styleable.SettingsSeek_Settings_progress, 70)
+        a.recycle()
+
+        vSeekBar.setOnSeekBarChangeListener(this)
+
+        setMaxProgress(maxProgress)
+        vSeekBar.progress = progress
+        isFocusable = false
+    }
+
+    //
+    //  State
+    //
+
+    public override fun onSaveInstanceState(): Parcelable? {
+        val bundle = Bundle()
+        bundle.putParcelable("SUPER_STATE", super.onSaveInstanceState())
+        bundle.putInt("progress", vSeekBar.progress)
+        return bundle
+    }
+
+    public override fun onRestoreInstanceState(s: Parcelable?) {
+        var state = s
+        if (state is Bundle) {
+            val bundle = state as Bundle?
+            progress = bundle!!.getInt("progress")
+            state = bundle.getParcelable("SUPER_STATE")
+        }
+        super.onRestoreInstanceState(state)
+    }
+
+    //
+    //  Setters
+    //
+
+    fun setMaxProgress(max: Int) {
+        vSeekBar.max = max
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        vSeekBar.isEnabled = enabled
+    }
+
+    fun setOnProgressChanged(onProgressChanged: (Int) -> Unit) {
+        this.onProgressChanged = onProgressChanged
+    }
+
+    //
+    //  Events
+    //
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+        if (onProgressChanged != null)
+            onProgressChanged!!.invoke(progress)
+    }
+
+}
